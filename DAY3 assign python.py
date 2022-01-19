9ac74d74a853c2906589f93736eb4fc5dbea6035
Swetha-aa <97897211+Swetha-aa@users.noreply.github.com>
2022-01-19 20:55:22 +0530

    Add files via upload

---
+++ DAY3 assign python.py
@@ -0,0 +1,229 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+# user input
+name= input()
+
+
+# In[ ]:
+
+
+name
+
+
+# In[ ]:
+
+
+'Lets Upgrade 123'
+
+
+# In[ ]:
+
+
+name=input("please enter your name :")
+
+
+# In[ ]:
+
+
+please enter your age: 12
+
+
+# In[ ]:
+
+
+age
+
+
+# In[ ]:
+
+
+'12'
+
+
+# In[ ]:
+
+
+type(age)
+
+
+# In[ ]:
+
+
+str
+
+
+# In[ ]:
+
+
+condition=input("")
+
+
+# In[ ]:
+
+
+True
+
+
+# In[ ]:
+
+
+condition
+
+
+# In[ ]:
+
+
+'True'
+
+marks=input("Marks:")
+# marks
+
+# In[ ]:
+
+
+'89.90'
+
+
+# In[ ]:
+
+
+type(marks)
+
+
+# In[ ]:
+
+
+str
+
+
+# In[ ]:
+
+
+age=input("Please Enter your age:(in number)")
+
+
+# In[ ]:
+
+
+Please Enter your age: (in number)15
+
+
+# In[ ]:
+
+
+int(age)+2
+
+
+# In[ ]:
+
+
+24
+
+
+# In[ ]:
+
+
+age=input("Please Enter your age:(in number)")
+
+
+# In[ ]:
+
+
+Please Enter your age: (in number)15
+
+
+# In[ ]:
+
+
+age+2
+
+
+# In[ ]:
+
+
+17
+
+
+# In[ ]:
+
+
+chr(12)
+
+
+# In[ ]:
+
+
+'\x0c'
+
+
+# In[ ]:
+
+
+6.6-4.2
+
+
+# In[ ]:
+
+
+1.3999999999
+
+
+# In[ ]:
+
+
+round(9.6-7.2)
+
+
+# 2
+
+# In[ ]:
+
+
+round(9.6-4.1,3)
+
+
+# In[ ]:
+
+
+5.5
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
