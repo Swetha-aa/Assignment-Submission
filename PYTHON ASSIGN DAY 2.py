6fd007c98f7e1a3f399735098181b2a0e5f04e1a
Swetha-aa <97897211+Swetha-aa@users.noreply.github.com>
2022-01-19 09:36:41 +0530

    Add files via upload

---
+++ PYTHON ASSIGN DAY 2.py
@@ -0,0 +1,291 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[35]:
+
+
+# datatypes
+
+
+# In[36]:
+
+
+age=23
+
+
+# In[37]:
+
+
+type(age)
+
+
+# In[38]:
+
+
+marks=56.7
+
+
+# In[39]:
+
+
+type(marks)
+
+
+# In[40]:
+
+
+name="Lets Upgrade"
+
+
+# In[41]:
+
+
+type(name)
+
+
+# In[ ]:
+
+
+isTrue = True
+isFalse = False
+
+
+# In[53]:
+
+
+name
+
+
+# In[55]:
+
+
+name='Lets Upgrade 1'
+
+
+# In[56]:
+
+
+name
+
+
+# In[57]:
+
+
+age
+
+
+# In[ ]:
+
+
+age=45
+
+
+# In[ ]:
+
+
+marks=78.89
+
+
+# In[ ]:
+
+
+print ("Lets Upgrade")
+
+
+# In[68]:
+
+
+print(name)
+
+
+# In[69]:
+
+
+print('hey'+name)
+
+
+# In[ ]:
+
+
+age=2
+
+
+# In[ ]:
+
+
+country='India'
+
+
+# In[75]:
+
+
+print (name,age) 
+
+
+# In[76]:
+
+
+print('my name is:',name,'aged:',age)
+
+
+# In[ ]:
+
+
+# f string
+
+
+# In[80]:
+
+
+print (f"2+2")
+
+
+# In[81]:
+
+
+print (f"{2+2}")
+
+
+# In[82]:
+
+
+print (f"{2-6}")
+
+
+# In[83]:
+
+
+print (f"{78/56}")
+
+
+# In[84]:
+
+
+print (f"{78%56}")
+
+
+# In[85]:
+
+
+print (f"programming language : {'Python'}")
+
+
+# In[86]:
+
+
+'''
+
+'''
+
+
+# In[87]:
+
+
+print("""hi""")
+
+
+# In[88]:
+
+
+print('''hi''')
+
+
+# In[89]:
+
+
+print(f'''{2+2}''')
+
+
+# In[ ]:
+
+
+total_items= list() #[]
+
+
+# In[ ]:
+
+
+total_items.append(89)
+
+
+# In[ ]:
+
+
+total_items.append(34.5)
+
+
+# In[ ]:
+
+
+total_items.append('Lets Upgrade')
+
+
+# In[ ]:
+
+
+total_items.append([1,2,3,4,5])
+
+
+# In[93]:
+
+
+min({1,5,8,9,6})
+
+
+# In[94]:
+
+
+max({4,8,6,3,7})
+
+
+# In[95]:
+
+
+sum({9,6,4,1,2})
+
+
+# In[97]:
+
+
+-18/4
+
+
+# In[98]:
+
+
+-18//4
+
+
+# In[99]:
+
+
+18//4
+
+
+# In[100]:
+
+
+18/4
+
+
+# In[ ]:
+
+
+tuple_a = tuple()
+
+
+# In[102]:
+
+
+type('type_a')
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
