d266af57bf8fe3092a75b2b50794a15666081ea9
Swetha-aa <97897211+Swetha-aa@users.noreply.github.com>
2022-01-20 21:20:20 +0530

    Add files via upload

---
+++ day4.py assign.py
@@ -0,0 +1,28 @@
+# Online Python compiler (interpreter) to run Python online.
+# Write Python 3 code in this online editor and run it.
+print("Hello world")
+
+# name = 'Lets Upgrade'
+# age =2
+# location = 'Mumbai'
+
+#print(f"my name is :{name}:{age} and stays at :{location}")
+
+#name = 'Lets Upgrade 2'
+# age =6
+# location = 'Dallas'
+
+# print (f"my name is :{name}:{age} and stays at :{location}")
+
+def introduction(name,age,location):
+    print(f"my name is :{name}:{age} and stays at :{location}")
+    
+    #print(type(introduction))
+    #introduction('python',22,'Delhi')
+    #introduction('Javascript',45,'Kolkata')
+    
+    def iterate_over_loop(1):
+        for i in 1:
+            print(f"my name :{i}")
+            
+            
